3a155e5e63aae0b44fe7cacebc46017efe893cde
jiewa404 <wangjie@vcyber.com>
2016-07-25 15:36:31 +0800

    微信自定义菜单 控制器

---
+++ backend/controllers/TopicController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/25
+ * Time: 15:12
+ */
+namespace  backend\controllers;
+
+use Yii;
+use yii\web;
+
+class TopicController extends BaseController{
+
+    /*
+    * 设置自定义菜单
+    */
+    public function actionMenu(){
+      
+
+    }
+
+
+}
